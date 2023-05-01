adcff9666f9eeacfbf4f45e25d2d4a94e09cffe8
Tiago <tiago.silva991@outlook.com>
2023-05-01 18:53:18 -0300

    ativar itens do orcamento

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// ativar links do menu 
+
 const links = document.querySelectorAll('.header-menu a');
 
 function ativarLink(link) {
@@ -11,4 +13,17 @@ function ativarLink(link) {
 
 
 links.forEach(ativarLink);
+
+// Ativar Itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto)
 
